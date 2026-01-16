a155ead99b0dff40543d75af93cbd54420fd4af8
NoamRothschild <noamr.lego@gmail.com>
2026-01-16 10:04:56 +0200

    feat: exposed runner to module

--- src/module.zig
+++ src/module.zig
@@ -1,5 +1,6 @@
 pub const Context = @import("CPU/context.zig").Context;
 pub const parser = @import("parser/root.zig");
 pub const executor = @import("CPU/executor.zig");
+pub const runner = @import("runner.zig");
 pub const ExecError = @import("errors.zig").ExecError;
 pub const ParseError = @import("errors.zig").ParseError;
